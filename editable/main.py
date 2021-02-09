1150993b4c51c5008266f5146c99a96c7e67f5f7
Rustum Chhor <rustum@Rustums-MacBook-Pro.local>
2021-02-09 13:44:42 -0800

    working version, but images are not cleaned before the end

--- editable/main.py
+++ editable/main.py
@@ -1,12 +1,45 @@
 import os
 import img2pdf
 import argparse
+import pathlib
+import glob
+
+from natsort import natsorted, ns
+import cv2
+import datetime
 from utils import *
+
+class input_file:
+	inputRegistry = []
+	def __init__(self, file_path):
+		self.filePath = file_path
+		self.lectureName = pathlib.Path(file_path).stem
+		self.fileExt = pathlib.Path(file_path).suffix
+		self.si_dirPath = ''
+		self.con_dirPath = ''
+		# input_file._inputRegistry.append(self)
 
 
 def main():
-	input_path = params.get('f')
+	new_input = input_file(params.get('f'))
 
+	# now = datetime.datetime.now()
+	# datetime.datetime.strftime(now, '%m/%d/%Y')
+
+	new_input.si_dirPath = 'temp/single_images'
+	pathlib.Path(new_input.si_dirPath).mkdir(parents=True, exist_ok=True)
+	pdf_to_images(file_path=new_input.filePath, output_directory=new_input.si_dirPath, lecture_name=new_input.lectureName)
+
+	graph = cv2.imread("templates/graph_paper/graph1.png")
+	graph_resized = cv2.resize(graph, (1815, 2754))
+	graph_vert = cv2.vconcat([graph_resized,graph_resized])
+	graph_hor = cv2.hconcat([graph_vert,graph_vert])
+
+	new_input.con_dirPath = 'temp/concat_images'
+	pathlib.Path(new_input.con_dirPath).mkdir(parents=True, exist_ok=True)
+	single_jpeg_to_5_graph_overlayed(input_directory=new_input.si_dirPath, lecture_name=new_input.lectureName, output_directory=new_input.con_dirPath, x_offset=0, y_offset=0, paper=graph_hor)
+
+	write_pdf(input_directory=new_input.con_dirPath, lecture_name=new_input.lectureName)
 
 
 if __name__ == "__main__":
--- editable/utils.py
+++ editable/utils.py
@@ -1,47 +1,42 @@
 import os
 import img2pdf
-import argparse
+import pdf2image
 import tempfile
 import datetime
+import pathlib
+import glob
 
+from natsort import natsorted, ns
+import cv2
+import datetime
+from utils import *
 
-
-
-def divide_list(full_list, n_way): 
+def divide_list(lst, n_way): 
     # looping till length list
-    for i in range(0, len(full_list), n_way):
-        yield full_list[i:i + n_way]
+    for i in range(0, len(lst), n_way):
+        yield lst[i:i + n_way]
 
 
 # print single images, return output directory
-def pdf_to_single_jpeg(file_path, output_directory, lecture_name):
-	images_from_path = convert_from_path(file_path, output_folder=output_directory)
-	for i, image in enumerate(images_from_path):
-	    fname = lecture_name+str(i)+'.jpeg'
-	    image.save(fname, "JPEG")
+def pdf_to_images(file_path, output_directory, lecture_name):
+	images_from_path = pdf2image.convert_from_path(file_path, output_folder=output_directory, fmt='jpeg', output_file=lecture_name)
+	# for i, image in enumerate(images_from_path):
+	#     fname = lecture_name+str(i)+'.jpg'
+	#     image.save(fname)
 
-
-def writeimages(num,x_offset,y_offset,paper):
-	images = [cv2.resize(cv2.imread(file), (1920, 1080)) for file in natsorted(glob.glob("lectures/{}/*.jpg".format(num)))]
-	subList = list(divide_list(images, 5))
-	for i in range(len(subList)):
-		im_v = cv2.vconcat(subList[i])
-		overlayed = paper
-		overlayed[y_offset:y_offset+im_v.shape[0], x_offset:x_offset+im_v.shape[1]] = im_v
-		if not cv2.imwrite("concatenated_lectures/lec_{}_pt_{}.jpeg".format(num, i), overlayed):
-			raise Exception("Could not write image")
 
 # input: directory of single images
 # loop through single images, print concat images
 # return output dir
-def single_jpeg_to_5_graph_overlayed(input_directory, lecture_name, output_directory, x_offset=0, y_offset=0, paper):
-	images = [cv2.resize(cv2.imread(file), (1920, 1080)) for file in natsorted(glob.glob("{}/*.jpeg".format(input_directory)))]
+def single_jpeg_to_5_graph_overlayed(input_directory, lecture_name, output_directory, paper, x_offset=0, y_offset=0):
+	images = [cv2.resize(cv2.imread(file), (1920, 1080)) for file in natsorted(glob.glob("{}/*.jpg".format(input_directory)))]
+	print(input_directory,len(images))
 	subList = list(divide_list(images, 5))
 	for i, image in enumerate(subList):
 		im_v = cv2.vconcat(subList[i])
 		overlayed = paper
 		overlayed[y_offset:y_offset+im_v.shape[0], x_offset:x_offset+im_v.shape[1]] = im_v
-		fname = lecture_name+str(i)+'.jpeg'
+		fname = lecture_name+str(i)+'.jpg'
 		if not cv2.imwrite("{}/{}".format(output_directory, fname), overlayed):
 			raise Exception("Could not write image")
 
@@ -49,13 +44,14 @@ def single_jpeg_to_5_graph_overlayed(input_directory, lecture_name, output_direc
 # input: directory of concated images
 # loop through concat images
 # print concatenated in single pdf output
-## note needed return: output file_path?
+## not needed return: output file_path?
 def write_pdf(input_directory, lecture_name):
-	fname = lecture_name+'output'+'.pdf'
+	fname = lecture_name+'_'+'output'+'.pdf'
 	with open(fname, "wb") as f:
 		imgs = []
 		for fname in os.listdir(input_directory):
-			if not fname.endswith(".jpeg"):
+			print(fname)
+			if not fname.endswith(".jpg"):
 				continue
 			path = os.path.join(input_directory, fname)
 			if os.path.isdir(path):
@@ -63,24 +59,3 @@ def write_pdf(input_directory, lecture_name):
 			imgs.append(path)
 			imgs = natsorted(imgs)
 		f.write(img2pdf.convert(imgs))
-
-
-
-def writepdf():
-	# convert all files ending in .jpeg inside a directory
-	dirname = "concatenated_lectures/"
-	with open("2.pdf","wb") as f:
-		imgs = []
-		for fname in os.listdir(dirname):
-			if not fname.endswith(".jpeg"):
-				continue
-			path = os.path.join(dirname, fname)
-			if os.path.isdir(path):
-				continue
-			imgs.append(path)
-			imgs = natsorted(imgs)
-		f.write(img2pdf.convert(imgs))
-
-
-
-
